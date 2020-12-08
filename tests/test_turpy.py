92c5b9ea29bb3d6b2720dfa65ef0e212b3a2f57f
Jose Beltran <drjobel.connection@gmail.com>
2020-12-08 17:57:41 +0100

    v0.1.2

--- tests/test_turpy.py
+++ tests/test_turpy.py
@@ -2,7 +2,7 @@ from turpy import __version__
 from turpy.io.load_yaml import load_yaml
 
 def test_version():
-    assert __version__ == '0.1.1'
+    assert __version__ == '0.1.2'
 
 def test_load_yaml(filepath: str = 'tests/test_yaml.yaml'):
     """ Tests load_yaml for `filepath` string"""
