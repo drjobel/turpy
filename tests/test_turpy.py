ebe61f69e835b9942dec625063db6b1652aa3e34
Jose Beltran <drjobel.connection@gmail.com>
2020-12-10 17:06:54 +0100

    Extended load_yaml to handle file type

--- tests/test_turpy.py
+++ tests/test_turpy.py
@@ -3,15 +3,19 @@ import os
 from turpy.io.load_yaml import load_yaml
 from turpy.utils import script_as_module
 
+from io import BytesIO, StringIO, IOBase
+from typing import Union
+
 # def test_version():
 #    assert __version__ == 0.1.3
 
-def test_load_yaml(filepath: str = 'tests/test_yaml.yaml'):
+
+def test_load_yaml(file: Union[str, BytesIO, StringIO] = 'tests/test_yaml.yaml'):
     """ Tests load_yaml for `filepath` string"""
-    assert filepath is not None
-    assert isinstance(filepath, str)
-    assert load_yaml(filepath=filepath)
-    my_dict = load_yaml(filepath=filepath)
+    assert file is not None
+    assert isinstance(file, str)
+    assert load_yaml(file=file)
+    my_dict = load_yaml(file=file)
     assert my_dict is not None
     assert isinstance(my_dict, dict)
 
